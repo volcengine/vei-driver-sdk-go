b7726eea2b4225d9adf007df4b51ece44773f0a8
ZhangZhe <zhangzhe.z@bytedance.com>
2024-03-30 22:54:19 +0800

    refactor: update the definition of driver interfaces
    Signed-off-by: ZhangZhe <zhangzhe.z@bytedance.com>

--- pkg/interfaces/driver.go
+++ pkg/interfaces/driver.go
@@ -17,29 +17,27 @@
 package interfaces
 
 import (
-	sdkmodels "github.com/edgexfoundry/device-sdk-go/v2/pkg/models"
-
-	"github.com/volcengine/vei-driver-sdk-go/pkg/log"
-	"github.com/volcengine/vei-driver-sdk-go/pkg/models"
+	"github.com/volcengine/vei-driver-sdk-go/pkg/contracts"
+	"github.com/volcengine/vei-driver-sdk-go/pkg/logger"
 )
 
 // Driver is a minimal interface to interact with a specific class of devices.
 type Driver interface {
 	// Initialize performs protocol-specific initialization for the device service.
 	// The given *AsyncValues channel can be used to push asynchronous readings to Core Data.
-	Initialize(logger log.Logger, asyncCh chan<- *models.AsyncValues) error
+	Initialize(logger logger.Logger, asyncCh chan<- *contracts.AsyncValues) error
 	// ReadProperty passes a slice of ReadRequest each representing a read operation for the specific device resource.
 	// The device service has the flexibility to set the final result of the request.
 	// SetResult: set the value read by driver instance.
 	// Failed: set error encountered when performing a read operation.
 	// Skip: just skip the request.
-	ReadProperty(device *models.Device, reqs []models.ReadRequest) error
+	ReadProperty(device *contracts.Device, reqs []contracts.ReadRequest) error
 	// WriteProperty passes a slice of WriteRequest each representing a write operation for the specific device resource.
 	// The parameter can be obtained through the Param function.
-	WriteProperty(device *models.Device, reqs []models.WriteRequest) error
+	WriteProperty(device *contracts.Device, reqs []contracts.WriteRequest) error
 	// CallService passes a CallRequest which representing a service call operation for the specific device resource.
 	// The payload can be obtained through the Payload function.
-	CallService(device *models.Device, reqs []models.CallRequest) error
+	CallService(device *contracts.Device, reqs []contracts.CallRequest) error
 	// Stop instructs the protocol-specific DS code to shut down gracefully, or
 	// if the force parameter is 'true', immediately. The driver is responsible
 	// for closing any in-use channels, including the channel used to send async
@@ -50,17 +48,17 @@ type Driver interface {
 // DeviceHandler is an optional interface to handle the system event of device
 type DeviceHandler interface {
 	// AddDevice is a callback function that is invoked when a new device associated with this driver is added
-	AddDevice(device *models.Device) error
+	AddDevice(device *contracts.Device) error
 	// UpdateDevice is a callback function that is invoked when a device associated with this driver is updated
-	UpdateDevice(device *models.Device) error
+	UpdateDevice(device *contracts.Device) error
 	// RemoveDevice is a callback function that is invoked when a device associated with this driver is removed
-	RemoveDevice(device *models.Device) error
+	RemoveDevice(device *contracts.Device) error
 }
 
 // TODO: Discovery is an optional interface implemented by driver that support dynamic device discovery.
 type Discovery interface {
 	// Discover triggers protocol specific device discovery.
-	Discover() []sdkmodels.DiscoveredDevice
+	Discover() []*contracts.Device
 }
 
 // TODO: Debugger is an optional interface implemented by driver that support debugging for device profile.
