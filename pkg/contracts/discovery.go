25c04a23492705db3a7af09cd995a383555ab729
ZhangZhe <zhangzhe.z@bytedance.com>
2024-04-28 20:09:40 +0800

    fix(discovery): complete the fields in parameter with json tag
    Signed-off-by: ZhangZhe <zhangzhe.z@bytedance.com>

--- pkg/contracts/discovery.go
+++ pkg/contracts/discovery.go
@@ -17,6 +17,8 @@
 package contracts
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -34,36 +36,76 @@ type DiscoveryParameter struct {
 	MulticastParameter  *MulticastParameter  `json:"multicast_parameter,omitempty"`
 	NetScanParameter    *NetScanParameter    `json:"net_scan_parameter,omitempty"`
 	SerialScanParameter *SerialScanParameter `json:"serial_scan_parameter,omitempty"`
+	Credentials         map[string]string    `json:"credentials,omitempty"`
 }
 
 // MulticastParameter is the input configuration for a Multicast Discovery.
 type MulticastParameter struct {
 	// The target ethernet interface for multicast discovering
-	EthernetInterface string
+	EthernetInterface string `json:"ethernet_interface"`
 }
 
 // NetScanParameter is the input configuration for a NetScan Discovery.
 type NetScanParameter struct {
 	// Protocol is the type of probe to make: tcp, udp, etc.
-	Protocol string
+	Protocol string `json:"protocol"`
 	// List of IPv4 subnets to perform netscan discovery on, in CIDR format (X.X.X.X/Y).
-	Subnets []string
+	Subnets []string `json:"subnets"`
 	// ScanPorts is a slice of ports to scan for on each host.
-	ScanPorts []string
+	ScanPorts []string `json:"scan_ports"`
 	// Maximum simultaneous network probes when running netscan discovery.
-	ProbeAsyncLimit int
+	ProbeAsyncLimit int `json:"probe_async_limit"`
 	// Maximum amount of milliseconds to wait for each IP probe before timing out.
-	ProbeTimeout time.Duration
+	ProbeTimeout time.Duration `json:"probe_timeout"`
 }
 
 // SerialScanParameter is the input configuration for a SerialScan Discovery.
 type SerialScanParameter struct {
 	// The serial port bitrate, e.g. 9600, 19200, ...
-	BaudRate int
+	BaudRate int `json:"baud_rate"`
 	// Size of the character (must be 5, 6, 7 or 8)
-	DataBits int
+	DataBits int `json:"data_bits"`
 	// The serial port stop bits setting (must be 1 or 2)
-	StopBits int
+	StopBits int `json:"stop_bits"`
 	// The serial port parity setting (must be none, even or odd)
-	Parity string
+	Parity string `json:"parity"`
+}
+
+func (dp *DiscoveryParameter) Valid() error {
+	switch dp.DiscoveryMode {
+	case Multicast:
+		if dp.MulticastParameter == nil || dp.MulticastParameter.EthernetInterface == "" {
+			return fmt.Errorf("multicast parameter invalid")
+		}
+	case NetScan:
+		if dp.NetScanParameter == nil {
+			return fmt.Errorf("netscan parameter invalid")
+		}
+		if len(dp.NetScanParameter.Subnets) == 0 || len(dp.NetScanParameter.ScanPorts) == 0 {
+			return fmt.Errorf("the subnets or scanports in netscan parameter can not be empty")
+		}
+		if dp.NetScanParameter.Protocol == "" {
+			dp.NetScanParameter.Protocol = "udp"
+		}
+		if dp.NetScanParameter.ProbeAsyncLimit == 0 {
+			dp.NetScanParameter.ProbeAsyncLimit = 4000
+		}
+		if dp.NetScanParameter.ProbeTimeout == 0 {
+			dp.NetScanParameter.ProbeTimeout = 2000
+		}
+	case SerialScan:
+		if dp.SerialScanParameter == nil || dp.SerialScanParameter.BaudRate == 0 ||
+			dp.SerialScanParameter.DataBits == 0 || dp.SerialScanParameter.StopBits == 0 ||
+			dp.SerialScanParameter.Parity == "" {
+			return fmt.Errorf("serialscan parameter invalid")
+		}
+	default:
+		return fmt.Errorf("discovery mode %s not supported", dp.DiscoveryMode)
+	}
+	return nil
+}
+
+func (dp *DiscoveryParameter) String() string {
+	data, _ := json.Marshal(dp)
+	return string(data)
 }
---
+++ pkg/contracts/discovery_test.go
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 Beijing Volcano Engine Technology Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package contracts
+
+import (
+	"testing"
+)
+
+func TestDiscoveryParameter_Valid(t *testing.T) {
+	tests := []struct {
+		name       string
+		mode       DiscoveryMode
+		multicast  *MulticastParameter
+		netscan    *NetScanParameter
+		serialscan *SerialScanParameter
+		wantErr    bool
+	}{
+		{name: "unsupported mode", mode: "other", wantErr: true},
+		{name: "invalid multicast", mode: Multicast, multicast: &MulticastParameter{EthernetInterface: ""}, wantErr: true},
+		{name: "valid multicast", mode: Multicast, multicast: &MulticastParameter{EthernetInterface: "eth0"}, wantErr: false},
+		{name: "nil netscan", mode: NetScan, netscan: nil, wantErr: true},
+		{name: "invalid netscan", mode: NetScan, netscan: &NetScanParameter{}, wantErr: true},
+		{name: "valid netscan", mode: NetScan, netscan: &NetScanParameter{Subnets: []string{"192.168.1.0/24"}, ScanPorts: []string{"3702"}}, wantErr: false},
+		{name: "nil serialscan", mode: SerialScan, serialscan: nil, wantErr: true},
+		{name: "invalid serialscan", mode: SerialScan, serialscan: &SerialScanParameter{}, wantErr: true},
+		{name: "valid serialscan", mode: SerialScan, serialscan: &SerialScanParameter{BaudRate: 9600, DataBits: 8, StopBits: 1, Parity: "none"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dp := &DiscoveryParameter{
+				DiscoveryMode:       tt.mode,
+				MulticastParameter:  tt.multicast,
+				NetScanParameter:    tt.netscan,
+				SerialScanParameter: tt.serialscan,
+			}
+			if err := dp.Valid(); (err != nil) != tt.wantErr {
+				t.Errorf("Valid() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestDiscoveryParameter_String(t *testing.T) {
+	dp := &DiscoveryParameter{
+		DiscoveryMode:      NetScan,
+		MaxDurationTime:    30,
+		MulticastParameter: &MulticastParameter{EthernetInterface: "eth0"},
+		NetScanParameter: &NetScanParameter{
+			Protocol:        "udp",
+			Subnets:         []string{"192.168.1.0/24"},
+			ScanPorts:       []string{"3702"},
+			ProbeAsyncLimit: 4000,
+			ProbeTimeout:    2000,
+		},
+		SerialScanParameter: &SerialScanParameter{
+			BaudRate: 9600,
+			DataBits: 7,
+			StopBits: 1,
+			Parity:   "none",
+		},
+	}
+	t.Log(dp.String())
+}
